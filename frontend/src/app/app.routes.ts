fa0618165d254fdb156028a691f29cc192eb225b
Rens Saladaga <staleexam19@gmail.com>
2024-12-02 10:51:21 +0800

    fix: incorrect path for 404 page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/views/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +14,9 @@ export const routes: Routes = [
     {
         path: "teacher",
         loadChildren: () => import("./teacher/teacher-routing.module").then(m => m.TeacherRoutingModule)
+    },
+    {
+        path: "**",
+        component: PageNotFoundComponent
     }
 ];
